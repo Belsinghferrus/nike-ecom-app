3cd7337779307af126f955cfd8a3524f2cce79e8
Belsinghferrus <ferrusbelsingh@gmail.com>
2024-09-14 10:27:13 +0530

    delete item from cart

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../models/cart.dart';
 import '../models/shoe.dart';
 
 class CartItem extends StatefulWidget {
@@ -8,17 +10,37 @@ class CartItem extends StatefulWidget {
     required this.shoe
     });
 
+
+
   @override
   State<CartItem> createState() => _CartItemState();
 }
 
 class _CartItemState extends State<CartItem> {
+
+  
+
+  void removeItemFromCart (){
+    Provider.of<Cart>(context, listen: false).removeItemFromCart(widget.shoe);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.asset(widget.shoe.imagepath),
-      title: Text(widget.shoe.name),
-      subtitle: Text(widget.shoe.price),
+    return Container(
+      decoration: BoxDecoration(color: Colors.grey[100],
+      borderRadius: BorderRadius.circular(10)
+      ),
+      margin: EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        leading: Image.asset(widget.shoe.imagepath),
+        title: Text(widget.shoe.name),
+        subtitle: Text(widget.shoe.price),
+        trailing: IconButton(
+          onPressed: removeItemFromCart,
+          icon: Icon(Icons.delete),
+        ),
+      ),
+
     );
   }
 }
