592407f0f566d7efe475133a530aa5ec99a5e2eb
Belsinghferrus <ferrusbelsingh@gmail.com>
2024-09-14 09:42:08 +0530

    alert on add cart

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -5,7 +5,8 @@ import '../models/shoe.dart';
 
 class ShoeTile extends StatelessWidget {
   Shoe shoe;
-   ShoeTile({super.key, required this.shoe});
+  void Function()? onTap;
+   ShoeTile({super.key, required this.shoe, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -41,13 +42,13 @@ class ShoeTile extends StatelessWidget {
                   children: [
                   //shoe name
                   Text(shoe.name,
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontSize: 28,
                     fontWeight: FontWeight.bold
                   ),
                   ),
                   //price
-                  Text("Rs  "+shoe.price,
+                  Text("Rs  ${shoe.price}/-",
                   style: TextStyle(
                     color: Colors.grey,
 
@@ -55,17 +56,20 @@ class ShoeTile extends StatelessWidget {
                   ),
                 ],
                 ),
-                Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(12),
+                GestureDetector(
+                  onTap: onTap,
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(12),
+                      ),
                     ),
+                      child: Icon(Icons.add,
+                      color: Colors.white,
+                      size: 30,
+                      ),
                   ),
-                    child: Icon(Icons.add,
-                    color: Colors.white,
-                    size: 30,
-                    ),
                 ),
               ],
             ),
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -13,6 +13,22 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
+
+  //add shoe to cart
+  void addShoeToCart(Shoe shoe){
+    Provider.of<Cart>(context, listen: false).addItemToCart(shoe);
+
+    //alert the user
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Successfully added to cart"),
+          content: Text("Check your cart"),
+
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<Cart>(builder: (context, value, child) => Column(
@@ -74,6 +90,7 @@ class _ShopPageState extends State<ShopPage> {
                 Shoe shoe = value.getShoeList()[index];
                 return ShoeTile(
                   shoe: shoe,
+                  onTap: () => addShoeToCart(shoe),
                 );
               }
           ),
